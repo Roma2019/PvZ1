f16a06edee4816f9c9b8e1346fb039e16fc56c1e
Roma2019 <47102574+Roma2019@users.noreply.github.com>
2019-06-04 17:02:15 +0900

    Add files via upload

---
+++ shpargalka.js
@@ -0,0 +1,26 @@
+let num = 100; //переменная- число
+let text ="text"; //переменная-текст
+let object = { //переменная - объект
+	key1:"что-то" ,//ключ со значением
+	key2:"что-то еще"
+onclick = function(){} // клики(нажатие) на различные html элементы
+}
+style.height = "100px"; //меняет различные css свойства элементов
+document.querySelector() // находит нужный элемент в html по классу 
+alert() //всплывающее окно на странице
+innerHTML = num; //заменяет содержимое элемента
+let proverka = function() {//создание функции
+	alert('privet')
+}
+setTimeout(proverka,1000) //таймер
+document.querySelectorAll() //находит все элементы с одинаковым классом
+let array = ["lol","kek",21,"hello"]; //массив
+for(let index = 0; index<2; index = index + 1){
+
+}
+kill = kill + 1
+      	document.querySelector(".kill").innerHTML = kill;
+
+	
+		kill1 = kill1 + 1
+      	document.querySelector(".kill1").innerHTML = kill1
